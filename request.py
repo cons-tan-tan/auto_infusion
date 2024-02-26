2574fd2d2ca5830b7b5cc4817af166df36c0fa17
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2024-02-26 17:12:59 +0900

    feat: add json formatter

---
+++ request.py
@@ -0,0 +1,43 @@
+import os
+import json
+
+import requests
+
+# ローカルで実行する場合は以下のコードを有効化する
+# from dotenv import load_dotenv
+# load_dotenv()
+
+url = "https://auto-infusion.microcms.io/api/v1/recipes"
+header= {
+    "content-type": "application/json",
+    "X-MICROCMS-API-KEY": os.getenv("MICROCMS_API_KEY"),
+}
+param = {
+    "fields": "product,amount,key,sub.key,essentia.name",
+    "limit": 100,
+}
+
+test = requests.get(url, headers=header, params=param).json()
+
+data = []
+
+for i in range(test["totalCount"]):
+    tmp = {}
+    recipe = test["contents"][i]
+    tmp["product"] = recipe["product"]
+    tmp["amount"] = recipe["amount"][0]
+    tmp["key"] = recipe["key"]
+    if recipe["sub"] != None:
+        tmp["sub"] = recipe["sub"]["key"]
+    essentia = []
+    for j in range(len(recipe["essentia"])):
+        tmp1 = {}
+        tmp1["aspect"] = recipe["essentia"][j]["aspect"]["name"]
+        tmp1["amount"] = recipe["essentia"][j]["amount"]
+        essentia.append(tmp1)
+    tmp["essentia"] = essentia
+    data.append(tmp)
+
+f = open('recipes.json', 'w')
+f.write(json.dumps(data, indent=4))
+f.close()
