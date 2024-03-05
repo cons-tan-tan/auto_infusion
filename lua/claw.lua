6bf9f22f4de0a67ea533e2be2c87d16cd4e3d29f
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2024-03-05 17:06:34 +0900

    feat: add craw script

---
+++ lua/claw.lua
@@ -0,0 +1,30 @@
+local component = require("component")
+local rs = component.redstone
+
+
+local direction = {
+    redstone = os.getenv("CRAW_REDSTONE_DIRECTION")
+    transposer = os.getenv("CRAW_TRANSPOSER_DIRECTION")
+}
+
+
+function init()
+    rs.setOutput(direction, 0)
+end
+
+function trigger()
+    rs.setOutput(direction, 15)
+end
+
+function isActive()
+    return rs.getOutput(direction) > 0
+end
+
+
+local craw = {
+    init = init,
+    trigger = trigger,
+    isActive = isActive
+}
+
+return craw
