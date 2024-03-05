f511d140786aebdeac0773ad576119a06c230b99
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2024-03-05 22:47:16 +0900

    feat: add wand check method

--- lua/claw.lua
+++ lua/claw.lua
@@ -1,6 +1,8 @@
 local component = require("component")
+local invoke = component.invoke
 local rs = component.redstone
 
+local adress = os.getenv("CRAW_TRANSPOSER_ADRESS")
 
 local direction = {
     redstone = tonumber(os.getenv("CRAW_REDSTONE_DIRECTION")),
@@ -8,21 +10,31 @@ local direction = {
 }
 
 
-function init()
+local function init()
     rs.setOutput(direction.redstone, 0)
 end
 
-function trigger()
-    rs.setOutput(direction.redstone), 15
+local function trigger()
+    rs.setOutput(direction.redstone, 15)
 end
 
-function isActive()
+local function isActive()
     return rs.getOutput(direction.redstone) > 0
 end
 
+local function isFine()
+    local vis = invoke(adress, "getStackInSlot", direction.transposer, 1).aspects
+    for i=1, 6 do
+        if vis[i].amount <= 1000 then --centi-vis単位
+            return false
+        end
+    end
+    return true
+end
 
 return {
     init = init,
     trigger = trigger,
-    isActive = isActive
+    isActive = isActive,
+    isFine = isFine
 }
