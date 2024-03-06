2aaa5963d8f695958d4a94b25741900d4f3f2a6b
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2024-03-07 04:48:38 +0900

    feat: add recipe module

---
+++ lua/recipe.lua
@@ -0,0 +1,38 @@
+local shell = require("shell")
+local json = require("json")
+
+local recipes = {}
+
+local function update()
+    shell.execute("wget -f https://raw.githubusercontent.com/cons-tan-tan/auto_infusion/main/recipes.json")
+    local file = io.open("recipes.json", "r")
+    if file == nil then
+        error("error: file not found", 0)
+    end
+    local data = file:read("a")
+    file:close()
+    recipes = json.decode(data)
+end
+
+local function isSub(item)
+    return recipes.sub[item]
+end
+
+local function getRecipe(item)
+    local recipe = recipes.recipes[item]
+    if recipe == nil then
+        print(item .. " : recipe not found")
+        update()
+        recipe = recipes.recipes[item]
+        if recipe == nil then
+            error("error: recipe not found", 0)
+        end
+    end
+    return recipe
+end
+
+return {
+    update = update,
+    isSub = isSub,
+    getRecipe = getRecipe
+}
