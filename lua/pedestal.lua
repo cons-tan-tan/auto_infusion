e7390451993dcccbdcda7a7f460a3a35c7df9b44
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2024-03-07 08:34:08 +0900

    feat: add module for pedestals

---
+++ lua/pedestal.lua
@@ -0,0 +1,38 @@
+local component = require("component")
+local invoke = component.invoke
+
+local adress = {
+    center = os.getenv("CENTER_TRANSPOSER_ADRESS"),
+    second = os.getenv("SECOND_TRANSPOSER_ADRESS")
+}
+
+local direction = {
+    center = tonumber(os.getenv("CENTER_TRANSPOSER_DIRECTION")),
+    second = tonumber(os.getenv("SECOND_TRANSPOSER_DIRECTION")),
+    output = tonumber(os.getenv("OUTPUT_TRANSPOSER_DIRECTION"))
+}
+
+local function getItem(adr, dir)
+    local item = invoke(adr, "getStackInSlot", dir, 1)
+    return item.label
+end
+
+local function getCenterItem()
+    return getItem(adress.center, direction.center)
+end
+
+local function getSecondItem()
+    return getItem(adress.second, direction.second)
+end
+
+local function exportCenterItems()
+    for i=1, invoke(adress.center, "getSlotStackSize", direction.center, 1) do
+        invoke(adress.center, "transferItem", direction.center, direction.output, 1, 1, 1)
+    end
+end
+
+return {
+    getCenterItem = getCenterItem,
+    getSecondItem = getSecondItem,
+    exportCenterItems = exportCenterItems
+}
