208bc9d93efad4f174ad39f7cdd998bcd64aa44b
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2024-03-29 07:42:16 +0900

    fix: fit for above the limit

--- request.py
+++ request.py
@@ -8,10 +8,12 @@ import traceback
 import requests
 
 
+LIMIT = 100
+
 def main():
     prepare_env()
     try:
-        data = serialize(get_recipe())
+        data = get_serialized_data()
         write_to_file(data)
     except Exception as e:
         post_to_discord()
@@ -45,7 +47,7 @@ def post_to_discord():
     requests.post(url, data=json.dumps(data).encode(), headers={"Content-Type": "application/json"})
 
 
-def get_recipe() -> dict:
+def get_recipe(offset) -> dict:
     url = os.environ.get("MICROCMS_SERVICE_DOMAIN")
     header= {
         "content-type": "application/json",
@@ -53,38 +55,42 @@ def get_recipe() -> dict:
     }
     param = {
         "fields": "product,amount,key,sub.key,essentia.name",
-        "limit": 100,
+        "limit": LIMIT,
+        "offset": offset,
     }
     return requests.get(url, headers=header, params=param).json()
 
 
-def serialize(raw_data) -> dict:
-    raw_data = get_recipe()
-    data = {}
-    data["recipe"] = {}
-    data["sub"] = {}
-
-    if raw_data["totalCount"] > 100:
-        raise Exception("レシピ総数がデフォルトの上限の100を超えました")
-    for i in range(raw_data["totalCount"]):
-        tmp = {}
-        recipe = raw_data["contents"][i]
-        tmp["product"] = recipe["product"]
-        amount = int(recipe["amount"][0])
-        if amount != 1:
-            tmp["amount"] = amount
-        if recipe["sub"] != None:
-            tmp["sub"] = recipe["sub"]["key"]
-            data["sub"][recipe["sub"]["key"]] = True
-        essentia = {}
-        for j in range(len(recipe["essentia"])):
-            aspect = recipe["essentia"][j]["aspect"]["name"].lower()
-            amount = recipe["essentia"][j]["amount"]
-            essentia[aspect] = amount
-        tmp["essentia"] = essentia
-        data["recipe"][recipe["key"]] = tmp
+def get_serialized_data() -> dict:
     now = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
-    data["updated_at"] = now.strftime("%Y-%m-%d %H:%M:%S")
+    data = {"recipe": {}, "sub": {}, "updated_at": now.strftime("%Y-%m-%d %H:%M:%S")}
+
+    raw_data = get_recipe(0)
+    total_count = raw_data["totalCount"]
+    i = 0
+    while True:
+        contents = raw_data["contents"]
+        for j in range(len(contents)):
+            tmp = {}
+            recipe = contents[j]
+            tmp["product"] = recipe["product"]
+            amount = int(recipe["amount"][0])
+            if amount != 1:
+                tmp["amount"] = amount
+            if recipe["sub"] != None:
+                tmp["sub"] = recipe["sub"]["key"]
+                data["sub"][recipe["sub"]["key"]] = True
+            essentia = {}
+            for j in range(len(recipe["essentia"])):
+                aspect = recipe["essentia"][j]["aspect"]["name"].lower()
+                amount = recipe["essentia"][j]["amount"]
+                essentia[aspect] = amount
+            tmp["essentia"] = essentia
+            data["recipe"][recipe["key"]] = tmp
+            i += 1
+        if i >= total_count:
+            break
+        raw_data = get_recipe(i)
 
     return data
 
